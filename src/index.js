e33042706337e2e9bb00c83b80608c94389976e6
jared kiprop <jaredassociates@gmail.com>
2025-06-25 00:16:05 +0300

    adding the display pages function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,25 @@ const BASE_URL = "http://localhost:3000/pages";
 
 document.addEventListener("DOMContentLoaded", main);
 
+function main() {
+  displayPages();
+  setupNewPageForm();
+}
+
+function displayPages() {
+  fetch(BASE_URL)
+    .then(res => res.json())
+    .then(pages => {
+      const list = document.getElementById("page-list");
+      list.innerHTML = "";
+      pages.forEach(page => {
+        const li = document.createElement("li");
+        li.textContent = page.title;
+        li.addEventListener("click", () => showPageDetail(page));
+        list.appendChild(li);
+      });
+
+      if (pages.length > 0) showPageDetail(pages[0]);
+    });
+}
+
